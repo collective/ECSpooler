6440562c0ca1a1965ab1ce6d7ee2510b3ba8dc33
Wolfram Fenske <wfenske@eudemonia-solutions.de>
2006-01-27 07:36:02 +0000

    AbstractCheck produced a deadlock on startup. Fixed.

--- lib/util/abstractCheck.py
+++ lib/util/abstractCheck.py
@@ -30,33 +30,25 @@ class AbstractCheck(object):
                 return fun(connection)
             finally:
                 connection.close()
-        finally:
+        finally:            
             self.lock.release()
         
-        
-    def tableExists(self):        
-        def fun(connection):
-            cursor = connection.cursor()
-            cursor.execute("SELECT name FROM sqlite_master WHERE TYPE='table'")
-            rows = cursor.fetchall()
-            cursor.close()
-            return len([row for row in rows
-                        if row["name"] == self.tableName]) > 0
-        
-        return self.withConnection(fun)
-
         
     def possiblyCreateTable(self):
         """
         checks whether needed table exists in databases.
         if it does not exist, it will be created.
         """
-        def fun(connection):
-            if not self.tableExists():
-                cursor = connection.cursor()
+        def fun(connection):            
+            cursor = connection.cursor()
+            cursor.execute("SELECT name FROM sqlite_master WHERE TYPE='table'")
+            rows = cursor.fetchall()
+            tableExists = len([row for row in rows
+                               if row["name"] == self.tableName]) > 0
+            if not tableExists:
                 cursor.execute("CREATE TABLE %s (%s)" % (self.tableName,
                                                     self.tableSchema))
-                cursor.close()
                 connection.commit()
+            cursor.close()
         
         return self.withConnection(fun)
