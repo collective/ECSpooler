987d8720c1935589380880099972889f2e2aa645
Mario Amelung <mario.amelung@gmx.de>
2006-10-18 14:41:46 +0000

    Initial check-in.

---
+++ backends/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# $Id$
+#
+# Copyright (c) 2006 Otto-von-Guericke-Universität, Magdeburg
+#
+# This file is part of ECSpooler.
+
+#subpackages
+
+#classes
+from backends.haskell.Haskell import Haskell
+from backends.haskell.Haskell import HaskellIO
+from backends.haskell.HaskellExtended import HaskellExtended
+
+from backends.java.Java import Java
