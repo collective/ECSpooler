c09c512fbcfd9ee233d99a5fea4a65218058afcf
Wolfram Fenske <wfenske@eudemonia-solutions.de>
2006-12-03 22:40:52 +0000

    replaced non-utf-8 character

--- lib/data/BackendResult.py
+++ lib/data/BackendResult.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # $Id$
 #
-# Copyright (c) 2006 Otto-von-Guericke-Universit�t Magdeburg
+# Copyright (c) 2006 Otto-von-Guericke-Universität Magdeburg
 #
 # This file is part of ECSpooler.
 
