aa3f55b59988259f755bbf99a6b0c501c1b7374d
Mario Amelung <mario.amelung@gmx.de>
2008-06-30 15:20:06 +0000

    *** empty log message ***

--- backends/junit/javac.sh
+++ backends/junit/javac.sh
@@ -1,14 +1,12 @@
 #! /bin/sh
 
-JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home; export JAVA_HOME
+JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.5/Home; export JAVA_HOME
 
 JAVA_BIN=${JAVA_HOME}/bin
 COMPILER=${JAVA_BIN}/javac
 
 JVM_HOME=${JAVA_HOME}; export JVM_HOME
 PATH=${JAVA_BIN}:${PATH}; export PATH
-
-#CLASSPATH=".:/Users/amelung/wdok/projekte/edu-components/impl/ECSpooler/backends/junit/junit_libs/junit-4.3.1.jar"
 
 ulimit -d `ulimit -H -d`
 
