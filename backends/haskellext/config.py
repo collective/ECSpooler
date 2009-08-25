3f1baf54214c599156be30ac12e1491d4f824a5b
Mario Amelung <mario.amelung@gmx.de>
2009-08-25 12:28:01 +0000

    Implemented current style guide for backends (i.e., config.py and explicit logging)

---
+++ backends/haskellext/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# $Id$
+#
+# Copyright (c) 2006 Otto-von-Guericke-Universität, Magdeburg
+#
+# This file is part of ECSpooler.
+
+from backends.haskell import config
+
+INTERPRETER = config.INTERPRETER
+#INTERPRETER = join(abspath(dirname(__file__)), 'runhugs+systrace')
+#INTERPRETER = join(abspath(dirname(__file__)), 'runhugs.sh')
