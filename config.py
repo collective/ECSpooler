61a1bf90b56eda22e1636fe9f6a2032176fee772
Mario Amelung <mario.amelung@gmx.de>
2009-08-25 12:34:17 +0000

    Reverted some changes from jel to ensure that spooler and backends are running on other systems than Solaris without 'magic' settings and conditions.

--- config.py
+++ config.py
@@ -1,171 +1,98 @@
 # -*- coding: utf-8 -*-
 # $Id$
 #
-# Copyright (c) 2005 Otto-von-Guericke-Universität Magdeburg
+# Copyright (c) 2005-2009 Otto-von-Guericke-Universität Magdeburg
 #
 # This file is part of ECSpooler.
-import sys, os, string, tempfile
+#
+import sys, os
 import logging.handlers
 
+# -- project properties -------------------------------------------------------
 PROJECTNAME = 'ECSpooler'
+
+
+# -- i18n properties ----------------------------------------------------------
 I18N_DOMAIN = 'eduComponents'
 
-# ------ default spooler properties -------------------------------------------
+
+# -- storage properties -------------------------------------------------------
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'var')
+
 JOB_QUEUE_STORAGE = os.path.join(DATA_DIR, 'data.fs')
 RESULT_QUEUE_STORAGE = os.path.join(DATA_DIR, 'result.fs')
 
-# ------ logging --------------------------------------------------------------
+
+# -- log properties -----------------------------------------------------------
 LOG_DIR = os.path.join(os.path.dirname(__file__), 'log')
-LOG_FORMAT  = logging.Formatter('%(asctime)s %(levelname)s: %(message)s ' \
-                             '(%(filename)s:%(lineno)d)')
+LOG_FORMAT  = logging.Formatter('%(asctime)s %(levelname)s: [%(name)s] %(message)s ' \
+                                '(%(filename)s:%(lineno)d)')
 #LOG_FORMAT = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 LOG_LEVEL = logging.DEBUG
 LOG_FILES = 7
-LOG_FILESIZE = 10 * 1024 * 1024 
+LOG_FILESIZE = 1024 * 1024 
 
-# ------ backend properties ---------------------------------------------------
+
+# -- additional properties ----------------------------------------------------
 # Remove temporary files and directories created by some backends
-CLEANUP = True
-
-RUN_AS = 'nobody'	# if started with uid change euid to this user
-RUN_AS_UID = 42		# fallback, if there is no pwd module to determine UID
-
-# -- all available backends (import names) ------------------------------------
-ALL_BACKENDS = 'erlang,haskell,haskellqc,haskellext,haskellio,java,junit,python,scheme,prolog,cl,javare,keywords'
+#CLEANUP = True 
+CLEANUP = False
 
 
-log = None
 
-##
-# Set the default data dir to use.
-# @param dir	default directory to use for storing jobs and results files. 
-#               Ignored if None or doesn't exist.
-def setDataDir(dir):
-     if not dir:
-         return
+# -- initialize ---------------------------------------------------------------
 
-     global DATA_DIR, JOB_QUEUE_STORAGE, RESULT_QUEUE_STORAGE
-     if os.path.exists(dir) and os.path.isdir(dir):
-         DATA_DIR = dir;
-         JOB_QUEUE_STORAGE = os.path.join(DATA_DIR, 'data.fs')
-         RESULT_QUEUE_STORAGE = os.path.join(DATA_DIR, 'result.fs')
-     else:
-         log.warn("var directory '%s' does not exist - using default '%s'" % (dir, DATA_DIR))
+# get directory for log files and create it if necessary
+if (not os.path.exists(LOG_DIR) and not os.path.isdir(LOG_DIR)):
+    print "log directory '%s' does not exist - trying to create it." % (LOG_DIR)
+    os.makedirs(LOG_DIR)
 
-##
-# Get the logging.level for the given name.
-# @param level	level name to convert.
-# @return None if invalid, the logging.level otherwise.
-def getLogLevel(level):
-    if not level:
-         return None
+# set the root logger (level, format and handlers)
+rch = logging.StreamHandler(sys.stdout)
+rfh = logging.handlers.RotatingFileHandler(os.path.join(LOG_DIR, 'event.log'),
+                                           'a', 
+                                           LOG_FILESIZE, 
+                                           LOG_FILES)
 
-    __lvl = level.upper()
-    if __lvl == "CRITICAL" or __lvl == "FATAL":
-        return logging.CRITICAL
-    elif __lvl == "ERROR":
-        return logging.ERROR
-    elif __lvl == "WARNING" or __lvl == "WARN":
-        return logging.WARNING
-    elif __lvl == "INFO":
-        return logging.INFO
-    elif __lvl == "DEBUG":
-        return logging.DEBUG
-    elif __lvl == "NOTSET" or __lvl == "NONE":
-        return logging.NOTSET
-    else:
-        return None
-##
-# Set the default log dir to use.
-# @param dir	default directory to use for storing log files. Will be created
-#               if it does not already exist. Ignored if None.
-def setLogDir(dir):
-    if not dir:
-        return
+rch.setFormatter(LOG_FORMAT)
+rfh.setFormatter(LOG_FORMAT)
 
-    global LOG_DIR
-    if os.path.exists(dir) and os.path.isdir(dir):
-	    LOG_DIR = dir
-    else:
-        log.warn("var directory '%s' does not exist - using default '%s'" % (dir, LOG_DIR));
+logging.getLogger('').setLevel(LOG_LEVEL)
+logging.getLogger('').addHandler(rfh)
+#logging.getLogger('').addHandler(rch)
 
-    if not os.path.exists(LOG_DIR): os.makedirs(LOG_DIR)
+# define special handlers for logging spooler messages
+sch = logging.StreamHandler(sys.stdout)
+sfh = logging.handlers.RotatingFileHandler(os.path.join(LOG_DIR, 'spooler.log'), 
+                                           'a', 
+                                           LOG_FILESIZE, 
+                                           LOG_FILES)
 
-##
-# Initialize logging.
-# @param level	log level to set.  Default: LOG_LEVEL
-# @param fname	basename of the logfile. If None, stderr will be used instead.
-# @param dir    directory, where to store the log.  Default: LOG_DIR
-# @param fsize	max. size of the rotating logfile. 
-# @param fcount	max. number of logfiles.  Default: LOG_FILES
-# @param format	log format to use.  Default: LOG_FORMAT
-# @return the log handler used
-def initLogging(level = None, fname = None, dir = None, fsize = None, fcount = None, format = None):
-    __log = logging.getLogger('')
-    if not level: level = LOG_LEVEL
-    __log.setLevel(level)
-    if not dir: dir = LOG_DIR
-    if not fsize: fsize = LOG_FILESIZE
-    if not fcount: fcount = LOG_FILES
-    if not format: format = LOG_FORMAT
-    if fname:
-        __handler = logging.handlers.RotatingFileHandler(
-                        os.path.join(dir, fname + '.log'), 'a', fsize, fcount)
-    else:
-        __handler = logging.StreamHandler()
+sch.setFormatter(LOG_FORMAT)
+sfh.setFormatter(LOG_FORMAT)
 
-    __handler.setFormatter(format)
-    __log.addHandler(__handler)
-    return __handler
+logging.getLogger('lib.Spooler').setLevel(LOG_LEVEL)
+logging.getLogger('lib.Spooler').addHandler(sfh)
+#logging.getLogger('lib.Spooler').addHandler(sch)
 
-def cleanup(enable):
-    global CLEANUP
-    CLEANUP = enable and enable.upper() == "TRUE"
+# define special handlers for logging backend messages
+bch = logging.StreamHandler(sys.stdout)
+bfh = logging.handlers.RotatingFileHandler(os.path.join(LOG_DIR, 'backends.log'), 
+                                           'a', 
+                                           LOG_FILESIZE, 
+                                           LOG_FILES)
 
-##
-# Try to set the UID of this process to the uid of the given user/uid.
-# @param user	user name to use to determine the uid
-# @param uid	uid to use, if uid can't be determined from user
-def runAs(user, uid):
-    if os.geteuid() != 0:
-        return
-    try:
-        import pwd
-        _uid, _gid = pwd.getpwnam(user)[2:4]
-    except ImportError:
-        _uid, _gid = 42, 42 
-   
-    try:
-        os.setuid(_uid)
-    except os.error, e:
-        log.warn("Cannot change uid: %s" % e)
+bch.setFormatter(LOG_FORMAT)
+bfh.setFormatter(LOG_FORMAT)
 
-##
-# Set the temp directory to use. If the application is not running as root,
-# this method tries to chmod 0700 the directory. Also it alters the environment
-# variable TMPDIR.
-# @param tmpdir		name of the directory to use for tempfiles. Ignored if None.
-def setTempDir(tmpdir):
-    if not tmpdir:
-        return
-    os.environ['TMPDIR'] = tmpdir
-    if not os.path.exists(tmpdir):
-        try:
-            os.makedirs(tmpdir, 0755)
-        except Exception, e:
-            log.warn("Unable to create directory: %s - using the default '%s' instead" % (e, tempfile.gettempdir()))
-        if os.geteuid() != 0 and tmpdir == tempfile.gettempdir():
-            """ Too much trouble one could cause as root """
-            try:
-                os.chmod(tmpdir, 0711)
-            except Exception, e:
-                log.warn("Unable to chmod 0711 %s: %s" % (tmpdir, e))
+logging.getLogger('backends').setLevel(LOG_LEVEL)
+logging.getLogger('backends').addHandler(bfh)
+#logging.getLogger('backends').addHandler(bch)
 
-def setWorkingDir(dir):
-    if not dir:
-        return
-    try:
-        os.chdir(dir)
-    except Exception, e:
-        log.warn("Unable to change working directory to '%s': %s" % (e, dir))
+
+# -- uid and gid for nobody ---------------------------------------------------
+try:
+    import pwd
+    NOBODY_UID, NOBODY_GID = pwd.getpwnam('nobody')[2:4]
+except ImportError:
+    NOBODY_UID, NOBODY_GID = 42, 42
